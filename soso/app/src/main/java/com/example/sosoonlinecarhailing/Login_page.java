6678a7f36d76280ee283f89c8d7b1318d38c9953
gi3636 <fenggi123@gmail.com>
2020-07-09 18:28:50 +0800

    配置了okhttp和QMUI

---
+++ soso/app/src/main/java/com/example/sosoonlinecarhailing/Login_page.java
@@ -0,0 +1,29 @@
+package com.example.sosoonlinecarhailing;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Login_page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_page);
+        Button loginBtn=findViewById(R.id.login_btn);
+        //设置点击监听器
+        loginBtn.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                //跳转界面，从login_page界面跳转去MainActivity界面
+                Intent intent=new Intent(Login_page.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
