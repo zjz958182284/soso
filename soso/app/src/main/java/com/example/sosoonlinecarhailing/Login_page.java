47545ec55abc39321f5a45ee8f65615c4d89b13f
gi3636 <fenggi123@gmail.com>
2020-07-11 23:36:29 +0800

    配置了高德地图API，增加了登入界面

--- soso/app/src/main/java/com/example/sosoonlinecarhailing/Login_page.java
+++ soso/app/src/main/java/com/example/sosoonlinecarhailing/Login_page.java
@@ -13,7 +13,7 @@ public class Login_page extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_page);
-        Button loginBtn=findViewById(R.id.login_btn);
+        Button loginBtn=findViewById(R.id.bt_login_submit);
         //设置点击监听器
         loginBtn.setOnClickListener(new View.OnClickListener(){
 
